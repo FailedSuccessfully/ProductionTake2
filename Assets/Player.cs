b7a695e39898b1931b9de8bdfa16ca2e8ff39662
FailedSuccessfully <itay.fichman@gmail.com>
2021-05-13 19:01:21 +0300

    Revert "Revert "oops more cleanup""
    This reverts commit 44ad554748aadbc10f74460ea2d2900f5c74b8f5.

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -18,14 +18,9 @@ public class Player : MonoBehaviour
 
 
     [SerializeField]
-    float maxSpeed, acceleration, deceleration, jumpForce, dashForce, boostForce, dashCooldown;
-    int setpAngle;
-    [SerializeField]
-    int jumpNum;
-    bool isMoving = false;
+    float dashForce, boostForce, dashCooldown;
     internal float currSpeed = 0, dashTime = 0;
     int currJump = 0;
-    protected Vector2 moveDirection = Vector2.zero;
     #endregion
     #region UrsulaFields
     public PelletController PelletPrefab;
@@ -45,7 +40,6 @@ public class Player : MonoBehaviour
             
             a = new PlayerMovement(this);
             b = new PlayerJump(this);
-            //Debug.Log(a.ComponentAction);
 
         }
 
