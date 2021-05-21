6a8babad0cd37a48c386d17ca5e299032a068ea2
FailedSuccessfully <itay.fichman@gmail.com>
2021-05-21 20:52:52 +0300

    stats custom drawer
    stats from scriptable objects will appear in player component editor

--- Assets/ScriptableObjects/PlayerStats.cs
+++ Assets/ScriptableObjects/PlayerStats.cs
@@ -1,8 +1,10 @@
-using System.Collections;
+using System;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 
 [CreateAssetMenu(fileName = "PlayerStats", menuName = "ScriptableObjects/PlayerStats", order = 1)]
+[Serializable]
 public class PlayerStats : ScriptableObject
 {
     [SerializeField]
@@ -10,3 +12,43 @@ public class PlayerStats : ScriptableObject
     [SerializeField]
     public int jumpNum, playerGravity;
 }
+
+
+// Custom Drawer
+[CustomPropertyDrawer(typeof(PlayerStats))]
+public class StatsDrawer : PropertyDrawer {
+    // Cached scriptable object editor
+    private Editor editor = null;
+ 
+    public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+    {
+        // Draw label
+        EditorGUI.PropertyField(position, property, label, true);
+        // Draw foldout arrow
+        if (property.objectReferenceValue != null)
+        {
+            property.isExpanded = EditorGUI.Foldout(position, property.isExpanded, GUIContent.none);
+        }
+ 
+        // Draw foldout properties
+        if (property.isExpanded)
+        {
+            // Make child fields be indented
+            EditorGUI.indentLevel++;
+         
+            // Draw object properties
+            if (!editor)
+                Editor.CreateCachedEditor(property.objectReferenceValue, null, ref editor);
+            editor.OnInspectorGUI();
+         
+            // Set indent back to what it was
+            EditorGUI.indentLevel--;
+        }
+    }
+}
+     
+    [CanEditMultipleObjects]
+    [CustomEditor(typeof(Player), true)]
+    public class PlayerEditor : Editor
+    {
+    }
