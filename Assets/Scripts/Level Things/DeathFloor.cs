d1d4aea1fa496569a589ee81cb5cd0bca30ba2fe
FailedSuccessfully <itay.fichman@gmail.com>
2021-07-03 20:54:54 +0300

    combine +

--- Assets/Scripts/Level Things/DeathFloor.cs
+++ Assets/Scripts/Level Things/DeathFloor.cs
@@ -7,6 +7,7 @@ public class DeathFloor : MonoBehaviour
     private void OnTriggerEnter2D(Collider2D other) {
         if (other.gameObject.layer == LayerMask.NameToLayer("Player")){
             GameManager.RespawnPlayer();
+            Debug.Log("mewody");
         }
     }
 }
