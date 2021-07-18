dd3488090dba000b895fe07b2e5409e20de2648e
FailedSuccessfully <itay.fichman@gmail.com>
2021-07-18 12:04:30 +0300

    lamp is animation

--- Assets/Scripts/Level Things/Checkpoint.cs
+++ Assets/Scripts/Level Things/Checkpoint.cs
@@ -4,11 +4,17 @@ using UnityEngine;
 
 public class Checkpoint : MonoBehaviour
 {
+    private Animator anim;
     public bool isActive{get; private set;}
+
+    private void Start() {
+        anim = GetComponent<Animator>();
+        SetActive(false);
+    }
 
     public void SetActive(bool active){
         this.isActive = active;
-        GetComponent<SpriteRenderer>().color = isActive ? Color.green : Color.red;
+        anim.SetBool("active", active);
     }
     private void OnTriggerEnter2D(Collider2D other) {
         if (other.gameObject.layer == LayerMask.NameToLayer("Player")){
